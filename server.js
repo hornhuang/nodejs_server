0e4502340bce4f7b58f8ccca5ef91b6b31fd4fb3
FishInWater-1999 <814484626@qq.com>
2019-10-12 11:45:12 +0800

    first commit

--- server.js
+++ server.js
@@ -7,11 +7,46 @@ var fs = require('fs')
 var path = require('path')
 
 var mimeModel = require('./model/getmimefromfile.js')
-
-
+// 用于将 GET/POST 提交上来的字符串转化为 query 形式
+var querystring = require('querystring')
 // http服务，网页访问时调用
 var app = http.createServer(function(req, res){
 
+    var data = []
+
+    req.on("error", function(err) {
+        // 错误处理
+        console.error(err)
+
+    }).on("data", function(chunk) {
+        // 将接收到的“表单”数据拼接到数组中
+        data.push(chunk) 
+        
+    }).on('end', function() {
+        // 判断请求类型
+        if (req.method == "POST") {
+            // 防止 POST 发送数据过多撑爆服务器
+            if(data.length > 1e6) {
+
+                req.connection.destroy()
+
+            }
+
+            data = Buffer.concat(data).toString();
+
+            console.log(querystring.parse(data))
+
+        }else {
+            // 处理 GET 请求
+            var params = url.parse(req.url, true).query
+
+            console.log("-------------------->" + params.name)
+            
+            console.log("-------------------->" + params.age)
+
+        }
+    })
+    
     var pathname = url.parse(req.url).pathname
     // 判断路由是否为：http://127.0.0.1:8000/ 这种，如果是赋值为主页 home.html 后面显示
     if(pathname == '/'){
